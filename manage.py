eccbb9610a9a052e97d8078bb707df8345215522
Luiz <junqueira.luizcarlo@gmail.com>
2015-08-08 15:38:28 -0300

    angula generics.RetrieveAPIVie

--- manage.py
+++ manage.py
@@ -8,7 +8,8 @@ import sys
 if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
 
     # import django
     # django.setup()
